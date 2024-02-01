23db0f7893b95166a4e6eb781cd4bda85329bffc
Benjamin Vankat <bvankat@gmail.com>
2024-02-01 11:26:44 -0600

    Removes top buttons, bottom ones working

--- js/copy-link.js
+++ js/copy-link.js
@@ -17,8 +17,8 @@ function CopyLink() {
 	
 	navigator.clipboard.writeText(url).then(function() {
 		console.log('Copied link: ', url);
+		document.getElementById('share-button-copy').innerHTML = "Link copied";
 	}, function() {
 		console.log('Copy error');
 	});
 };
-
